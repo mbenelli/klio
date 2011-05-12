a97cabc3fa6aa1aca6aad3527aa0170da8056ece
Marco Benelli <mbenelli@yahoo.com>
2011-05-13 01:45:09 +0200

    Removed maximum heap size for benchmarks.

--- benchmarks/kliows.sh
+++ benchmarks/kliows.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 cd ../klio
-gsi -:h10000 prelude lists charsets strings base64 datetime http kws -e '(kws#kws port-number: 8000 server-root: "../benchmarks/sandbox/" multithread: #f)'
+gsi prelude lists charsets strings base64 datetime http kws -e '(kws#kws port-number: 8000 server-root: "../benchmarks/sandbox/" multithread: #t)'
+
+
