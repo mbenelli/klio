219002e108bcec63432eedba9f1ff4a3b77a4313
Marco Benelli <mbenelli@yahoo.com>
2011-06-11 23:29:11 +0200

    Added namespace file for modbus.

---
+++ klio/modbus#.scm
@@ -0,0 +1,18 @@
+;; modbus.scm - Modbus protocol implementation.
+;;
+;; Copyright (c) 2011 by Marco Benelli <mbenelli@yahoo.com>
+;; All Right Reserved.
+;;
+;; Author: Marco Benelli <mbenelli@yahoo.com>
+;;
+
+(##namespace
+ ("modbus#"
+  read-coils!
+  read-inputs-discrete!
+  read-multiple-registers!
+  read-input-registers!
+  write-coil!
+  write-single-register!
+  read-exception-status!))
+
