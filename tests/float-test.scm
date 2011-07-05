3d270710f51690dddc372c3443dd842b94602b07
Marco Benelli <mbenelli@yahoo.com>
2011-07-05 09:09:19 +0200

    Added support from reading/writing floating points.

---
+++ tests/float-test.scm
@@ -0,0 +1,36 @@
+(include "../floats#.scm")
+(include "../floats.scm")
+
+(call-with-output-file
+  "f32test"
+  (lambda (port)
+    (write-f32 -1.5 port)
+    (write-f32 +inf.0 port)
+    (write-f32 3.1415926 port)))
+
+(call-with-input-file
+  "f32test"
+  (lambda (port)
+    (let* ((a (read-f32 port))
+           (b (read-f32 port))
+           (c (read-f32 port))
+           (d (read-f32 port)))
+      (pp (list a b c d)))))
+
+
+(call-with-output-file
+  "f64test"
+  (lambda (port)
+    (write-f64 -1.5 port)
+    (write-f64 +inf.0 port)
+    (write-f64 3.1415926 port)))
+
+(call-with-input-file
+  "f64test"
+  (lambda (port)
+    (let* ((a (read-f64 port))
+           (b (read-f64 port))
+           (c (read-f64 port))
+           (d (read-f64 port)))
+      (pp (list a b c d)))))
+
