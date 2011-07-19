ac645ba8bd98990fe33733bb9546e378b2e6956f
Marco Benelli <mbenelli@yahoo.com>
2011-07-20 01:43:00 +0200

    Added function fetch/apply. Work in progress of endianess-aware function for reading and writing.

--- tests/fwtest.scm
+++ tests/fwtest.scm
@@ -6,26 +6,70 @@
 ;;; Author: Marco Benelli <mbenelli@yahoo.com>
 ;;;
 
-(##namespace ("fwtest#"))
-(##include "~~lib/gambit#.scm")
+(##include "../klio/ctypes#.scm")
 (##include "../klio/fetchwrite#.scm")
 
-;; (define fetch-port
-;;   (open-tcp-client
-;;     `(server-address: "192.168.1.32" port-number: 2000)))
+(define address (make-parameter "localhost"))
 
-;; (define res (fetch-db 62 0 250 fetch-port))
-
-;; (pp res)
-;; (close-port fetch-port)
-
-(define (run)
+(define (test-fetch-db)
   (let* ((fp (open-tcp-client
-               `(server-address: "192.168.1.32" port-number: 2000)))
+               `(server-address: ,(address) port-number: 2000)))
          (res (fetch-db 62 0 250 fp)))
     (pp res)
     (close-port fp)))
 
+; TODO: clean up, test alternatives and move all the endianess-aware code
+; to ctypes.
+
+(define native-endianess (make-parameter 'little))
+
+(define (read-f32vector n port endianess)
+  (let* ((res (make-f32vector n))
+	 (float-buffer (make-u8vector 4))
+	 (push-value
+	   (if (eq? endianess (native-endianess))
+	       (lambda (i)
+		 (vector-set! res i (read-f32 port)))
+	       (lambda (i)
+		 (u8vector-set! float-buffer 3 (read-u8 port))
+		 (u8vector-set! float-buffer 2 (read-u8 port))
+		 (u8vector-set! float-buffer 1 (read-u8 port))
+		 (u8vector-set! float-buffer 0 (read-u8 port))
+		 (f32vector-set! res i (with-input-from-u8vector
+					 float-buffer
+					 read-f32))))))
+    (do ((i 0 (+ i 1)))
+      ((= i n) res)
+      (push-value i))))
+      
+
+(define-type
+  query0
+
+  measures
+  alarms
+  enablings
+  prms)
+
+
+(define (read-db p)
+  (let ((measures (make-vector 32))
+	(alarms (make-u8vector 10))
+	(enablings (make-u8vector 16))
+	(prms (make-vector 24)))
+    (set! measures (read-f32vector 32 p 'big))
+    (read-subu8vector alarms 0 10 p)
+    (read-subu8vector enablings 0 16 p)
+    (set! prms (read-f32vector 24 p 'big))
+    (make-query0 measures alarms enablings prms)))
+
+
+(define (test-fetch/apply)
+  (let ((p (open-tcp-client
+	     `(server-address: ,(address) port-number: 2000))))
+    (fetch/apply 62 0 250 read-db p)))
+
+(define q0 (call-with-input-u8vector r read-db))
 
 ;; Sample output.
 
