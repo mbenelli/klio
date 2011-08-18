3bcc0023efa159c6e1e837b152a518898fc75342
Marco Benelli <mbenelli@yahoo.com>
2011-08-18 16:27:01 +0200

    Added 'da' runtime option for debugging purpose.

--- benchmarks/kliows.sh
+++ benchmarks/kliows.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 cd ../klio
-gsi prelude lists base64 datetime http kws -e '(kws#kws port-number: 8000 server-root: "../benchmarks/sandbox/" multithread: #t)'
+gsi -:da prelude lists charsets strings base64 datetime http kws -e '(kws#kws port-number: 8000 server-root: "../benchmarks/sandbox/" multithread: #t)'
 
 
