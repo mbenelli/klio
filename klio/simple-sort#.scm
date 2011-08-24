1d9e99044fddc3b94bd40dc784a0dce2cdb69791
Marco Benelli <mbenelli@yahoo.com>
2011-08-24 17:12:19 +0200

    Added simple-sort module.

---
+++ klio/simple-sort#.scm
@@ -0,0 +1,7 @@
+;; simple-sort.scm - A simple implementation of merge sort.
+;;
+;; Copyright (c) 2011 by Marco Benelli <mbenelli@yahoo.com>
+;; All Rights Reserved.
+;;
+
+(##namespace ("simple-sort#" sort))
