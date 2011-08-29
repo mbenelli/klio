e4be8deec397e2520529d7df4184fe4912c903a0
Marco Benelli <mbenelli@yahoo.com>
2011-08-29 17:59:45 +0200

    Added some modules to compilation scripts.

--- build.scm
+++ build.scm
@@ -14,6 +14,7 @@
     datetime
     format
     vectors
+    simple-sort
     shift-reset
 
     queue
@@ -32,6 +33,9 @@
     http-auth
     sessions
     cgi
+
+    modbus
+    fetchwrite
     ))
 
 (parameterize ((current-directory dir))
