151f8471ab120f61cfc906c1c7720b8527b3df93
Marco Benelli <mbenelli@yahoo.com>
2012-02-11 16:34:51 +0100

    Added rfc 1123 date format.

---
+++ klio/rfc1123#.scm
@@ -0,0 +1,10 @@
+; rfc1123#.scm - date format namespace
+;
+; Copyright (c) 2012 by Marco Benelli <mbenelli@yahoo.com>
+; All Rights Reserved.
+
+(##namespace
+ ("rfc1123#"
+  seconds->string
+  time->string))
+
