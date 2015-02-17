3e288639882a94403b62dfed28e62876fd677df2
Marco Benelli <mbenelli@yahoo.com>
2015-02-17 18:37:09 +0100

    Fix launcher script used in benchmarks.

--- benchmarks/kliows.sh
+++ benchmarks/kliows.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 
-cd ../klio
-gsi -:da prelude lists charsets strings base64 datetime http kws -e '(kws#kws port-number: 8000 server-root: "../benchmarks/sandbox/" multithread: #t)'
+export KLIO_PATH=../klio
 
+gsi -:da \
+$KLIO_PATH/prelude \
+$KLIO_PATH/lists \
+$KLIO_PATH/charsets \
+$KLIO_PATH/strings \
+$KLIO_PATH/base64 \
+$KLIO_PATH/datetime \
+$KLIO_PATH/rfc1123 \
+$KLIO_PATH/uri \
+$KLIO_PATH/http-srv \
+$KLIO_PATH/kws \
+-e '(kws#kws port-number: 8000 server-root: "./sandbox" multithread: #t)'
 
